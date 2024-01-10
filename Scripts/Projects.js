721586deb8d7e691745f9c6065ec0335a432d3aa
FrederikH22R <43875606+frederikvase@users.noreply.github.com>
2024-01-10 11:08:11 +0100

    Foldable subtasks :)

--- Scripts/Projects.js
+++ Scripts/Projects.js
@@ -1,5 +1,5 @@
-var itemCount;
 var projects;
+var projectName;
 
 class Project
 {
@@ -8,6 +8,7 @@ class Project
         this.name = arr[0];
         this.time = parseFloat(arr[1]);
         this.date = arr[2];
+        this.content = arr[3];
     }
 }
 
@@ -17,80 +18,161 @@ function reload()
     if (typeof (Storage) !== "undefined") 
     {
         if (!localStorage.projects)
-            localStorage.projects = "[]";
-    }
-
-    itemCount = JSON.parse(localStorage.projects).length;
-    projects = [];
-    
-    for (let i = 0; i < itemCount; i++)
-    {
-        projects.push(JSON.parse(localStorage.projects)[i]);
-        showItem(projects[i]);
-    }
-
-    // Delete closed list items
-    let close = document.getElementsByClassName("close");
-    for (let i = 0; i < close.length; i++)
-    {
-        let div = close[i].parentElement;
-        const index = Array.from(div.parentNode.children).indexOf(div);
-
-        close[i].onclick = function() 
         {
-            list.remove(index); 
-        } 
+            localStorage.projects = JSON.stringify(new Project(["", 0, "", Array(0)]));
+        }
+    }
+
+    //itemCount = JSON.parse(localStorage.projects).length;
+    projects = JSON.parse(localStorage.projects);
+    
+    for (let i = 0; i < projects.content.length; i++)
+    {
+        document.getElementById("myUL").appendChild(getProjectDiv(projects.content[i]));
+        document.getElementById("myUL").appendChild(getContentDiv(projects.content[i]));
+
+        console.log(document.getElementById("myUL"));
+        //showItem(projects.content[i]);
     }
 }
 
-function showItem(task)
+function getProjectDiv(project)
 {
-    let table = document.createElement("table");
-    let row = table.insertRow();
-    document.getElementById("myUL").appendChild(table);
+    let div = document.createElement("div");
+    div.className = "project";
+    div.innerHTML = project.name;
 
-    // Display date in leftmost cell
-    let leftCell = row.insertCell();
-    leftCell.textContent = new Date(task.date).toLocaleDateString("en-GB");
-    leftCell.style.width = "25%"; 
-    row.appendChild(leftCell);
-
-    // Display name in middle cell
-    let middleCell = row.insertCell();
-    middleCell.textContent = task.name;
-    middleCell.style.width = "50%";
-    row.appendChild(middleCell);
-
-    // Create a table cell for labour time
-    let rightCell = row.insertCell();
-    rightCell.textContent = task.time;
-    rightCell.style.width = "25%";
-    row.appendChild(rightCell);
+    let button = document.createElement("button");
+    button.className = "collapsibleButton";
+    button.innerHTML = "BTN";
+    button.onclick = function() { showContent(this); };
+    
+    div.appendChild(button);
+    return div;
 }
 
-function createItem()
+function getContentDiv(project)
 {
-    let name = "Epic name"; // document.getElementById("name").value;
-    let time = "100"; // document.getElementById("time").value;
-    let date = "(Do)Due date"; // new Date(document.getElementById("date").value).toLocaleDateString("ne");
+    let content = document.createElement("div");
+    content.className = "content";
 
-    if (name == '' || time == '' || date == '')
+    if (typeof(project.content) !== "undefined")
     {
-        alert("Fill out the entire thing, you SAUCY boy!");
+        for (let i = 0; i < project.content.length; i++)
+        {
+            content.appendChild(getProjectDiv(project.content[i]));
+            content.appendChild(getContentDiv(project.content[i]));
+        }
+    }
+    
+    let projectAdder = document.createElement("div");
+    projectAdder.className = "project";
+    projectAdder.innerHTML = "Add new project!";
+
+    projectAdder.onclick = function() { 
+        openAddProjectModal(project.name) 
+    };
+    content.appendChild(projectAdder);
+
+    return content;
+}
+
+function openAddProjectModal(name)
+{
+    projectName = name;
+    let modal = document.getElementById("AddProjectModal");
+    modal.style.display = "block";
+}
+
+function addProject()
+{
+    let modal = document.getElementById("AddProjectModal");
+    modal.style.display = "block";
+
+    document.getElementById("projectAdder").style.display = "block";
+    document.getElementById("subProjectAdder").style.display = "none";
+
+    let name = document.getElementById("projectName").value;
+    let date = new Date(document.getElementById("date").value).toLocaleDateString("ne");
+    let time = "100";
+
+    if (name == '')
+    {
+        alert("Please fill out a name for the project!");
     }
     else if (typeof(Storage) !== "undefined")
     {
-        const element = new Project([name, time, date]);
-
-        // Add the new project to projects array
-        projects.push(element);
-        itemCount++;
+        const element = new Project([name, time, date, Array(0)]);
 
         // Add new project to localStorage
-        let copy = localStorage.projects != "" ? JSON.parse(localStorage.projects) : [];
-        copy.push(element);
-        localStorage.projects = JSON.stringify(copy);
+        projects.content.push(element);
+        localStorage.projects = JSON.stringify(projects);
 
         location.reload();
+    }
+}
+
+function addSubProject()
+{
+    let modal = document.getElementById("AddProjectModal");
+    modal.style.display = "block";
+
+    if (document.getElementById("projectAdder").style.display === "block")
+        document.getElementById("projectAdder").style.display = "none";
+    if (document.getElementById("subProjectAdder").style.display === "none")
+        document.getElementById("subProjectAdder").style.display = "block";
+
+    let name = document.getElementById("projectName").value;
+    let date = new Date(document.getElementById("date").value).toLocaleDateString("ne");
+    let time = "100";
+
+    if (name == '')
+    {
+        alert("Please fill out a name for the project!");
+    }
+    else if (typeof(Storage) !== "undefined")
+    {
+        const element = new Project([name, time, date, Array(0)]);
+
+        // Add new project to localStorage
+        addElementToProjects(projects, projectName, element);
+        localStorage.projects = JSON.stringify(projects);
+
+        location.reload();
+    }
+}
+
+function addElementToProjects(project, name, element)
+{
+    if (typeof(project.content) === "undefined")
+        return;
+
+    alert(name);
+
+    for (let i = 0; i < project.content.length; i++)
+    {
+        if (project.content[i].name == name)
+        {
+            //alert("aaa");
+            project.content[i].content.push(element);
+            return;
+        }
+
+        addElementToProjects(project.content[i], name, element);
+    }
+}
+
+function showContent(btn)
+{
+    btn.parentElement.classList.toggle("active");
+    var content = btn.parentElement.nextElementSibling;
+
+    if (content.style.display === "block") 
+    {
+        content.style.display = "none";
+    } 
+    else 
+    {
+        content.style.display = "block";
     }
 }
